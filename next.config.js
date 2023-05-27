5ea48f7c90057f9eb368304970b973e6ccbcf4db
Shiuan Chou <im.cps0903@gmail.com>
2023-05-27 15:24:07 +0800

    調整 - ESLint、移除預設畫面

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable eol-last */
+/* eslint-disable object-curly-newline */
+/* eslint-disable quote-props */
+/* eslint-disable semi */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
