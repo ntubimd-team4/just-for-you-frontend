a23094f8eac44373b20d304ee8940f3cdf9a1728
Scott Lee <10744038@ntub.edu.tw>
2023-05-28 22:30:46 +0800

    新增 - Overlay元件

---
+++ components/10744038/Menu.tsx
@@ -0,0 +1,29 @@
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button,
+} from '@chakra-ui/react';
+
+export default function MyMenu() {
+  return (
+    <Menu m={10}>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+    Actions
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Download</MenuItem>
+        <MenuItem>Create a Copy</MenuItem>
+        <MenuItem>Mark as Draft</MenuItem>
+        <MenuItem>Delete</MenuItem>
+        <MenuItem>Attend a Workshop</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
--- pages/10744038/index.tsx
+++ pages/10744038/index.tsx
@@ -1,5 +1,11 @@
 import MyAccordion from '@/components/10744038/Accordion';
+import MyAlertDialog from '@/components/10744038/AlertDialog';
+import MyDrawer from '@/components/10744038/Drawer';
+import MyMenu from '@/components/10744038/Menu';
+import MyModal from '@/components/10744038/Modal';
+import MyPopover from '@/components/10744038/Popover';
 import MyTabs from '@/components/10744038/Tabs';
+import MyTooltip from '@/components/10744038/Tooltip';
 import MyVisuallyHidden from '@/components/10744038/VisuallyHidden';
 import { Container } from '@chakra-ui/react';
 
@@ -9,6 +15,12 @@ export default function Page10744038() {
       <MyAccordion />
       <MyTabs />
       <MyVisuallyHidden />
+      <MyAlertDialog />
+      <MyDrawer />
+      <MyMenu />
+      <MyModal />
+      <MyPopover />
+      <MyTooltip />
     </Container>
   );
 }
