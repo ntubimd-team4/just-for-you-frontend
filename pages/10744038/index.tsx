0578f6276dbfa7b43ed3711b21e2664509d8c35e
scottkuanlee <97860440+scottkuanlee@users.noreply.github.com>
2023-05-28 15:41:57 +0800

    新增 - Accordion, Tabs, Visually Hidden (#3)

---
+++ pages/10744038/index.tsx
@@ -0,0 +1,14 @@
+import MyAccordion from '@/components/10744038/Accordion';
+import MyTabs from '@/components/10744038/Tabs';
+import MyVisuallyHidden from '@/components/10744038/VisuallyHidden';
+import { Container } from '@chakra-ui/react';
+
+export default function Page10744038() {
+  return (
+    <Container>
+      <MyAccordion />
+      <MyTabs />
+      <MyVisuallyHidden />
+    </Container>
+  );
+}
