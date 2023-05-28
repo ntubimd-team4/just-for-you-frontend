8a25c62c45fb67b929c4923124d8a7239266c106
chichi0219 <128296282+chichi0219@users.noreply.github.com>
2023-05-28 15:41:45 +0800

    新增 - Accordion,Tabs,VisuallyHidden - 元件 (#4)

---
+++ pages/10946038/index.tsx
@@ -0,0 +1,14 @@
+import MyAccordion from '@/components/10946038/Accordion';
+import MyTads from '@/components/10946038/Tabs';
+import MyVisuallyHidden from '@/components/10946038/VisuallyHidden';
+import { Container } from '@chakra-ui/react';
+
+export default function Page10946038() {
+  return (
+    <Container>
+      <MyAccordion />
+      <MyTads/>
+      <MyVisuallyHidden/>
+    </Container>
+  );
+}
