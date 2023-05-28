f5a23dd605cd9905179ffd3d6881054d1160774a
周珮宣 <69238128+ShiuanChou@users.noreply.github.com>
2023-05-29 00:46:38 +0800

    新增 - Navbar、Footer (#6)
    Co-authored-by: Shiuan Chou <10946011@ntub.edu.tw>

--- components/Layout/Navbar/index.tsx
+++ components/Layout/Navbar/index.tsx
@@ -1,28 +1,84 @@
-import { Flex, Spacer } from '@chakra-ui/react';
-import { Box, Container, Heading } from '@chakra-ui/layout';
-import { Button, ButtonGroup } from '@chakra-ui/button';
-import Link from 'next/link';
+import { ReactNode } from 'react';
+import {
+  Box, Flex, Avatar, HStack, Link, IconButton, Button, Menu, MenuButton, MenuList, MenuItem,
+  MenuDivider, useDisclosure, useColorModeValue, Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
-export default function Navbar() {
+const Links = ['樹洞', '推薦', 'Team'];
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      'textDecoration': 'none',
+      'bg': useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
+
+export default function Simple() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <nav>
-      {/* <Container> */}
-      <Flex minWidth="max-content" alignItems="center" gap="2">
-        <Box p="2">
-          <Heading size="md">Chakra App</Heading>
-        </Box>
-        <Spacer />
-        <Box p="2">
-          <Link href={'/tree'}>樹洞</Link>
-          <Link href={'/tree'}>樹洞</Link>
-          <Link href={'/tree'}>樹洞</Link>
-        </Box>
-        <ButtonGroup gap="2">
-          <Button colorScheme="teal">Sign Up</Button>
-          <Button colorScheme="teal">Log in</Button>
-        </ButtonGroup>
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <IconButton
+          size={'md'}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label={'Open Menu'}
+          display={{ 'md': 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack spacing={8} alignItems={'center'}>
+          <Box>諮屬於你</Box>
+          <HStack
+            as={'nav'}
+            spacing={4}
+            display={{ 'base': 'none', 'md': 'flex' }}>
+            {Links.map(link => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex alignItems={'center'}>
+          <Menu>
+            <MenuButton
+              as={Button}
+              rounded={'full'}
+              variant={'link'}
+              cursor={'pointer'}
+              minW={0}>
+              <Avatar
+                size={'sm'}
+                src={
+                  'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                }
+              />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Link 1</MenuItem>
+              <MenuItem>Link 2</MenuItem>
+              <MenuDivider />
+              <MenuItem>登出</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
       </Flex>
-      {/* </Container> */}
-    </nav>
+
+      {isOpen ? (
+        <Box pb={4} display={{ 'md': 'none' }}>
+          <Stack as={'nav'} spacing={4}>
+            {Links.map(link => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
   );
 }
