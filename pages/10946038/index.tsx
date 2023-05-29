dff298065268fac227bbed4139f1457b4baa9ad3
chichi0219 <10946038@ntub.edu.tw>
2023-05-29 12:45:53 +0800

    新增 - Alert,CirculearPorgress,Progress,Skeleton,Spinner,Tozat - 元件

--- pages/10946038/index.tsx
+++ pages/10946038/index.tsx
@@ -1,14 +1,25 @@
 import MyAccordion from '@/components/10946038/Accordion';
+import MyAlert from '@/components/10946038/Alert';
+import MyCircularProgress from '@/components/10946038/CircularProgress';
+import MyProgress from '@/components/10946038/Progress';
+import MySkeleton from '@/components/10946038/Skeleton';
+import MySpinner from '@/components/10946038/Spinner';
 import MyTads from '@/components/10946038/Tabs';
+import MyToast from '@/components/10946038/Toast';
 import MyVisuallyHidden from '@/components/10946038/VisuallyHidden';
 import { Container } from '@chakra-ui/react';
 
 export default function Page10946038() {
   return (
-    <Container>
+    <><Container>
       <MyAccordion />
-      <MyTads/>
-      <MyVisuallyHidden/>
-    </Container>
+      <MyTads />
+      <MyVisuallyHidden />
+      <MyAlert />
+      <MyCircularProgress />
+      <MyProgress />
+      <MySkeleton />
+      <MySpinner />
+    </Container></>
   );
 }
