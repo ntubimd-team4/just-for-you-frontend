cd902b1e42f1d4ac802305abae5dad745e78befa
chichi0219 <10946038@ntub.edu.tw>
2023-05-30 19:05:20 +0800

    新增 - Avatar,Icon,Image,Breadcrumb,Link - 元件

---
+++ components/10946038/Breadcrumb.tsx
@@ -0,0 +1,26 @@
+import {
+  Accordion,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from '@chakra-ui/react';
+
+export default function MyBreadcrumb(){
+  return (
+    <Breadcrumb>
+      <BreadcrumbItem>
+        <BreadcrumbLink href="#">Home</BreadcrumbLink>
+      </BreadcrumbItem>
+
+      <BreadcrumbItem>
+        <BreadcrumbLink href="#">Docs</BreadcrumbLink>
+      </BreadcrumbItem>
+
+      <BreadcrumbItem isCurrentPage>
+        <BreadcrumbLink href="#">Breadcrumb</BreadcrumbLink>
+      </BreadcrumbItem>
+    </Breadcrumb>
+
+  );
+}
--- pages/10946038/index.tsx
+++ pages/10946038/index.tsx
@@ -1,13 +1,18 @@
 import MyAccordion from '@/components/10946038/Accordion';
 import MyAlert from '@/components/10946038/Alert';
+import MyAvatar from '@/components/10946038/Avatar';
+import MyBreadcrumb from '@/components/10946038/Breadcrumb';
 import MyCircularProgress from '@/components/10946038/CircularProgress';
+import MyIcon from '@/components/10946038/Icon';
+import MyImage from '@/components/10946038/Image';
+import MyLink from '@/components/10946038/Link';
 import MyProgress from '@/components/10946038/Progress';
 import MySkeleton from '@/components/10946038/Skeleton';
 import MySpinner from '@/components/10946038/Spinner';
 import MyTads from '@/components/10946038/Tabs';
 import MyToast from '@/components/10946038/Toast';
 import MyVisuallyHidden from '@/components/10946038/VisuallyHidden';
-import { Container } from '@chakra-ui/react';
+import { Container, Icon } from '@chakra-ui/react';
 
 export default function Page10946038() {
   return (
@@ -20,6 +25,11 @@ export default function Page10946038() {
       <MyProgress />
       <MySkeleton />
       <MySpinner />
+      <MyAvatar/>
+      <MyIcon/>
+      <MyImage/>
+      <MyBreadcrumb/>
+      <MyLink/>
     </Container></>
   );
 }
