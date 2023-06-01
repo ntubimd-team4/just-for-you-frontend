0d112936726655d4d242ec4b4fb65848e52af506
Shiuan Chou <10946011@ntub.edu.tw>
2023-06-01 21:28:40 +0800

    調整 - 登入畫面

---
+++ pages/login/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout';
+import LoginBtn from '@/components/LoginBtn';
+
+export default function Login() {
+  return (
+    <Layout headTitle={'登入'}>
+      <LoginBtn />
+    </Layout>
+  );
+}
