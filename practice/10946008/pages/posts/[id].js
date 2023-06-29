7a4763701e5c04eefba9d3bcd86b77274c91b336
Luc1ne <128296965+Luc1ne@users.noreply.github.com>
2023-06-29 10:27:17 +0800

    新增 - Next.js練習 (#23)
    * 新增 - NEXTJS練習
    
    * 新增 - Next.js練習

--- practice/10946008/pages/posts/[id].js
+++ practice/10946008/pages/posts/[id].js
@@ -1,17 +1,25 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -22,10 +30,5 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
+  // params.id will be like ['a', 'b', 'c']
 }
