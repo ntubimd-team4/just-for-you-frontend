bc070b8df9853d8fbd35e6e9e92167633080ea8a
周珮宣 <69238128+ShiuanChou@users.noreply.github.com>
2023-08-31 21:17:56 +0800

    修改 - userAccount api 路徑、改axios版本 (#49)
    * 新增 - Google登入
    
    * 調整 - Google登入
    
    * 調整 - 畫面排版
    
    * 新增 - 路徑權限
    
    * 新增 - 學生、諮商師功能(樹洞、帳號管理)
    
    * 刪除 - 未用到的檔案
    
    * 調整 - Layout component
    
    * 調整 - 登入畫面
    
    * 調整 - 權限導頁
    
    * 調整 - 錯誤訊息
    
    * 調整 - type錯誤
    
    * 調整 - 個人帳號頁面
    
    * 移除 - 練習檔案
    
    * 調整 - 科系文字、type
    
    * 新增 - 練習資料夾
    
    * 目錄調整
    
    * 新增 - Spline畫面設計初版
    
    * 調整 - user api檔名
    
    * 刪除 - 未用到檔案
    
    * 修改 - userAccount api 路徑、改axios版本
    
    ---------
    
    Co-authored-by: Shiuan Chou <10946011@ntub.edu.tw>

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,7 +1,13 @@
-import NextAuth from 'next-auth';
+import { Account } from 'next-auth';
 
 declare module 'next-auth' {
   interface Session {
-    id_token?: string
+    id_token?: Account.id_token
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id_token?: Account.id_token
   }
 }
