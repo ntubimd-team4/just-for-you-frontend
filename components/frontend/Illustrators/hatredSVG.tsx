95a2030f3995070c644ebe3b30a5a8cb5b7abc6d
chichi0219 <128296282+chichi0219@users.noreply.github.com>
2023-10-21 16:50:58 +0800

    新增 - 暴怒,悲傷,憎恨,驚悚,崇敬 插圖 (#58)
    * 新增 - Accordion,Tabs,VisuallyHidden - 元件
    
    * 新增 - Alert,CirculearPorgress,Progress,Skeleton,Spinner,Tozat - 元件
    
    * 新增 - Avatar,Icon,Image,Breadcrumb,Link - 元件
    
    * 新增 - Next.js練習
    
    * 刪除 - 元件
    
    * 新增 - next.js
    
    * 文件 - 新增 - Next.js練習
    
    * 新增 - 文件 - js型態
    
    * 新增 - 文件 - js練習2
    
    * 新增 - 文件 - js練習2
    
    * 目錄調整
    
    * 文件 - 新增 - JS練習3
    
    * 新增 - 文件 -JS練習4
    
    * 文件 - 新增 - JS練習5
    
    * 新增 - 文件 - JS練習6
    
    * 新增 - 文件 - JS練習7
    
    * 新增 - 文件 - JS練習8
    
    * 新增 - 情緒API
    
    * 新增 - 驚愕插圖
    
    * 新增 - 快樂插圖
    
    * 新增 - 憤怒,憎恨,驚悚,崇敬,悲傷 插圖

---
+++ components/frontend/Illustrators/hatredSVG.tsx
@@ -0,0 +1,32 @@
+<svg width="516" height="579" viewBox="0 0 516 579" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g filter="url(#filter0_b_82_7)">
+    <path d="M150.596 558.213C150.44 546.974 159.424 537.737 170.663 537.581L212.712 536.997L213.029 559.869C213.166 569.715 205.295 577.808 195.449 577.944L171.228 578.281C159.989 578.437 150.752 569.452 150.596 558.213Z" fill="#26DD81"/>
+  </g>
+  <path d="M180.313 577.997V438.997H213.313V557.997C213.313 569.043 204.359 577.997 193.313 577.997H180.313Z" fill="#26DD81"/>
+  <g filter="url(#filter1_b_82_7)">
+    <path d="M301.317 536.863L343.365 536.279C354.604 536.123 363.842 545.108 363.998 556.347C364.154 567.586 355.169 576.823 343.93 576.979L319.71 577.316C309.864 577.452 301.771 569.581 301.634 559.735L301.317 536.863Z" fill="#26DD81"/>
+  </g>
+  <path d="M321.317 576.997C310.271 576.997 301.317 568.043 301.317 556.997V437.997H334.317V576.997H321.317Z" fill="#26DD81"/>
+  <path d="M35.2964 410.596C30.2897 400.027 34.7987 387.401 45.3675 382.394L136.942 339.013C147.511 334.007 160.137 338.516 165.144 349.084C170.15 359.653 165.641 372.279 155.073 377.286L63.498 420.667C52.9293 425.673 40.303 421.164 35.2964 410.596Z" fill="#26DD81"/>
+  <path d="M355.156 360.586C360.162 350.017 372.789 345.508 383.357 350.515L459.473 386.572C470.042 391.579 474.551 404.205 469.544 414.774C464.538 425.343 451.911 429.852 441.343 424.845L365.227 388.788C354.658 383.781 350.149 371.155 355.156 360.586Z" fill="#26DD81"/>
+  <path d="M407 263.519C407 461.4 407 461.4 261.245 461.4C115.489 461.4 111.844 461.4 109.881 387.061C109 353.71 109 313.061 109 263.519C109 173.431 177.162 100.4 261.245 100.4C345.327 100.4 407 173.431 407 263.519Z" fill="#A55EEC"/>
+  <circle cx="221.81" cy="234.571" r="25.5" fill="white"/>
+  <circle cx="222.31" cy="235.071" r="15" fill="#242424"/>
+  <circle cx="294.81" cy="234.571" r="25.5" fill="white"/>
+  <circle cx="295.31" cy="235.071" r="15" fill="#242424"/>
+  <path d="M319.495 339.063C319.495 339.063 295.995 284.571 248.495 311.571C200.995 338.571 199.995 339.063 199.995 339.063" stroke="#242424" stroke-width="10" stroke-linecap="round"/>
+  <defs>
+    <filter id="filter0_b_82_7" x="70.5938" y="456.997" width="222.437" height="201.286" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feGaussianBlur in="BackgroundImageFix" stdDeviation="40"/>
+      <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_82_7"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_82_7" result="shape"/>
+    </filter>
+    <filter id="filter1_b_82_7" x="221.317" y="456.277" width="222.683" height="201.04" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feGaussianBlur in="BackgroundImageFix" stdDeviation="40"/>
+      <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_82_7"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_82_7" result="shape"/>
+    </filter>
+  </defs>
+</svg>;
