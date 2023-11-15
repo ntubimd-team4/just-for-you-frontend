5342eb4b7667d8dac91cc04fd9ae878cd0cdf310
Shiuan Chou <10946011@ntub.edu.tw>
2023-11-15 09:10:12 +0800

    調整 - Google設定

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -6,9 +6,16 @@ export default NextAuth({
     GoogleProvider({
       'clientId': process.env.GOOGLE_CLIENT_ID as string,
       'clientSecret': process.env.GOOGLE_CLIENT_SECRET as string,
+      'authorization': {
+        'params': {
+          'prompt': 'consent',
+          'access_type': 'offline',
+          'response_type': 'code'
+        }
+      }
     })
   ],
-  'secret': process.env.JWT_SECRET,
+  'secret': process.env.NEXTAUTH_SECRET,
   'callbacks': {
     async jwt({ token, account }) {
       if (account) {
