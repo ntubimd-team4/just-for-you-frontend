c1e4a3f3f0cb1565a90a1914ac44493ef51e0c72
Shiuan Chou <10946011@ntub.edu.tw>
2023-11-15 09:11:23 +0800

    修改 - 摘要管理畫面、layout styles

--- pages/consultation-manage/[userId]/[sid].tsx
+++ pages/consultation-manage/[userId]/[sid].tsx
@@ -1,55 +1,114 @@
 import Layout from '@/components/backend/Layout';
 import summaryAPI from '@/services/summaryRecordAPI';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import styles from '@/styles/backend/_SummaryDetail.module.scss';
+import { useSession } from 'next-auth/react';
+import consultationAPI from '@/services/consultationAPI';
+
+type DetailType = {
+  userName: string,
+  role: string,
+  department: string,
+  establishTime: string,
+  topic: string,
+  summary: string,
+  content: string,
+  consultationContent: string,
+  sid: string,
+}
 
 export default function ConsultationRecord() {
   const router = useRouter();
+  const { status } = useSession();
   const { sid } = router.query;
-  const [detailData, setDetailData] = useState();
+  const [detailData, setDetailData] = useState<DetailType>();
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [recordContext, setRecordContext] = useState('');
 
   useEffect(() => {
-    const fetchData = async () => {
-      if (sid) {
-        try {
-          const response = await summaryAPI.getSummaryDetail(sid);
-          const data = response.data;
+    if (status === 'unauthenticated') {
+      router.push('/');
+    } else if (status === 'authenticated') {
+      const fetchData = async () => {
+        if (sid) {
+          try {
+            const response = await summaryAPI.getSummaryDetail(sid);
+            const data = response.data;
 
-          setDetailData(data);
-        } catch (error: any) {
-          alert(error.message);
+            setDetailData(data);
+            setRecordContext(data.consultationContent === null ? '' : data.consultationContent);
+          } catch (error: any) {
+            alert(error.message);
+          }
         }
+      };
+
+      fetchData();
+    }
+  }, [router, sid, status]);
+
+  const handleRecord = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setRecordContext(e.target.value);
+  };
+
+  const confirmEdit = (sid: string) => {
+    const editReord = async () => {
+      try {
+        const response = await consultationAPI.patchConsultation({ sid, 'content': recordContext });
+
+        await alert(response.message);
+        router.reload();
+      } catch (error: any) {
+        alert(error.message);
       }
     };
 
-    fetchData();
-  }, [sid]);
+    editReord();
+  };
+
+  const cancelEdit = () => {
+    setIsEditOpen(false);
+  };
 
   return (
     <Layout>
       <div className={styles.head}>
         <h1>摘要紀錄</h1>
-        <p>{detailData?.userName} {detailData?.role} {detailData?.department}</p>
+        <p>{detailData?.role}：{detailData?.userName}{' / '}{detailData?.department}</p>
       </div>
       <p>{detailData?.establishTime}</p>
       <p>諮商主題：{detailData?.topic}</p>
       <section className={styles.content}>
-        <div className={styles.item}>
-          <section className={styles.summary}>
+        <div className={styles.row}>
+          <section className={styles.item}>
             <h2>摘要：</h2>
             <p>{detailData?.summary}</p>
           </section>
-          <section className={styles.origin}>
+          <section className={styles.item}>
             <h2>原始訊息：</h2>
             <p>{detailData?.content}</p>
           </section>
         </div>
-        <div className={styles.item}>
-          <section className={styles.record}>
-            <h2>諮商師紀錄：</h2>
+        <div className={styles.row}>
+          <section className={styles.item}>
+            <section className={styles.record}>
+              <h2>諮商師紀錄：</h2>
+              {isEditOpen ? <textarea value={recordContext} onChange={e => handleRecord(e)} className={styles.recordArea} placeholder="請輸入諮商紀錄..." name="" id="" cols={30} rows={10} /> :
+                <p>{detailData?.consultationContent === null ? '尚無諮商紀錄' :
+                  detailData?.consultationContent}</p>
+              }
+            </section>
           </section>
-          <button className={styles.editBtn}>編輯紀錄</button>
+          {isEditOpen ?
+            <div>
+              <button className={styles.editBtn}
+                onClick={() => confirmEdit(detailData?.sid as string)}>
+                確定編輯
+              </button>
+              <button className={styles.editBtn} onClick={() => cancelEdit()}>取消編輯</button>
+            </div> :
+            <button className={styles.editBtn} onClick={() => setIsEditOpen(true)}>編輯紀錄</button>}
         </div>
       </section>
     </Layout>
--- pages/consultation-manage/[userId]/index.tsx
+++ pages/consultation-manage/[userId]/index.tsx
@@ -4,58 +4,77 @@ import Layout from '@/components/backend/Layout';
 import summaryAPI from '@/services/summaryRecordAPI';
 import styles from '@/styles/backend/_SummaryList.module.scss';
 import { Button } from '@chakra-ui/react';
+import { useSession } from 'next-auth/react';
+
+type DetailType = {
+  userName: string,
+  role: string,
+  department: string,
+  summaryRecordList: SummaryRecordType[],
+  userId: string,
+}
+
+type SummaryRecordType = {
+  establishTime: string,
+  topic: string,
+  summary: string,
+  sid: string,
+}
 
 export default function ConsultationDetail() {
   const router = useRouter();
+  const { status } = useSession();
   const { userId } = router.query;
-  const [detailData, setDetailData] = useState();
+  const [detailData, setDetailData] = useState<DetailType>();
 
   useEffect(() => {
-    const fetchSummaryData = async () => {
-      if (userId) {
-        try {
-          const response = await summaryAPI.getSummary(userId);
-          const data = response.data;
+    if (status === 'unauthenticated') {
+      router.push('/');
+    } else if (status === 'authenticated') {
+      const fetchSummaryData = async () => {
+        if (userId) {
+          try {
+            const response = await summaryAPI.getSummary(userId);
+            const data = response.data;
 
-          setDetailData(data);
-        } catch (error: any) {
-          alert(error.message);
+            setDetailData(data);
+          } catch (error: any) {
+            alert(error.message);
+          }
         }
-      }
-    };
+      };
 
-    fetchSummaryData();
-  }, [userId]);
+      fetchSummaryData();
+    }
+  }, [router, status, userId]);
 
   return (
     <Layout>
       <div className={styles.head}>
         <h1>摘要紀錄</h1>
-        <p>{detailData?.userName} {detailData?.role} {detailData?.department}</p>
+        <p>{detailData?.role}：{detailData?.userName}{' / '}{detailData?.department}</p>
       </div>
       <section>
         {detailData?.summaryRecordList.length === 0 ?
           <p>尚無紀錄</p> :
           detailData?.summaryRecordList.map((data, index) =>
-            <>
-              <article className={styles.list} key={index}>
-                <h2 className={styles.title}>{data.establishTime ? data.establishTime : '摘要時間'}</h2>
-                <div className={styles.content}>
-                  <section className={styles.text}>
-                    <p>來訪主題：{data.topic}</p>
-                    <p>摘要：{data.summary}</p>
-                  </section>
-                  <Button flex={1}
-                    fontSize={'sm'}
-                    rounded={'full'}
-                    _focus={{ 'bg': 'gray.200', }}
-                    aria-label="詳細記錄"
-                    onClick={() => router.push(`/consultation-manage/${detailData.userId}/${data.sid}`)}>
+            <article className={styles.list} key={index}>
+              <h2 className={styles.title}>{data.establishTime ? data.establishTime : '摘要時間'}</h2>
+              <div className={styles.content}>
+                <section className={styles.text}>
+                  <p>來訪主題：{data.topic}</p>
+                  <p>摘要：{data.summary}</p>
+                </section>
+                <Button flex={1}
+                  fontSize={'sm'}
+                  rounded={'full'}
+                  _focus={{ 'bg': 'gray.200', }}
+                  aria-label="詳細記錄"
+                  onClick={() => router.push(`/consultation-manage/${detailData.userId}/${data.sid}`)}>
                     詳細記錄
-                  </Button>
-                </div>
-              </article>
-            </>)
+                </Button>
+              </div>
+            </article>)
         }
       </section>
     </Layout>
