4e37de07c559eaa0e7a651373510b8dec0aca30b
Shiuan Chou <10946011@ntub.edu.tw>
2023-12-13 20:08:50 +0800

    新增 - 音樂列表元件、個人資料元件

---
+++ ts/types/MusicList.type.ts
@@ -0,0 +1,14 @@
+export type StoryResType = {
+    thumbnails: string,
+    link: string,
+    song: string,
+}
+
+export type PlayListType = {
+    rid: number;
+    isCollection: boolean;
+    emotionTag: number;
+    link: string;
+    song: string;
+    thumbnails: string;
+}
