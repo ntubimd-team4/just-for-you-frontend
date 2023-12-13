43c222e1331cee39f2cc34ac1e4dcef75a01ec63
Shiuan Chou <10946011@ntub.edu.tw>
2023-12-13 19:58:08 +0800

    調整 - type folder

---
+++ ts/interface/User.interface.ts
@@ -0,0 +1,9 @@
+export interface AccountListType {
+    userId?: string,
+    userName?: string,
+    userSex?: string,
+    department?: string,
+    picture?: string,
+    role?: string,
+    available?: boolean
+  }
