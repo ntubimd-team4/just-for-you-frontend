3941a8a81ed2d4e485f19fd99cbf5d5a1abc4593
Shiuan Chou <10946011@ntub.edu.tw>
2023-12-14 11:51:17 +0800

    修改 - 前臺收藏功能

--- components/frontend/MusicList/index.tsx
+++ components/frontend/MusicList/index.tsx
@@ -1,12 +1,39 @@
 import styles from '@/styles/frontend/_MusicList.module.scss';
-import { FaRegHeart } from 'react-icons/fa';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import YouTubeEmbed from '../YouTubeModal';
-import { StoryResType } from '@/ts/types/MusicList.type';
+import { PlayListType } from '@/ts/types/MusicList.type';
+import recommendAPI from '@/services/recommendAPI';
+import { useEffect, useState } from 'react';
 
-export default function MusicList({ musicData }: {musicData: StoryResType[]}) {
+export default function MusicList({ musicData }: {musicData: PlayListType[]}) {
+  const [listData, setListData] = useState<PlayListType[]>([]);
+
+  useEffect(() => {
+    if (musicData) {
+      setListData(musicData);
+    }
+  }, [musicData]);
+
+  const patchCollection = async (rid: number) => {
+    try {
+      await recommendAPI.patchCollention({ 'rid': rid });
+
+      const updatedArray = listData.map(item => {
+        if (item.rid === rid) {
+          return { ...item, 'isCollection': !item.isCollection };
+        }
+        return item;
+      });
+
+      await setListData(updatedArray);
+    } catch (err: any) {
+      alert(err.message);
+    }
+  };
+
   return (
     <section className={styles.playList}>
-      {musicData.map((music, index) => (
+      {listData.map((music, index) => (
         <article className={styles.list} key={index}>
           <div className={styles.numWrap}>
             <h3>{index + 1}</h3>
@@ -19,7 +46,10 @@ export default function MusicList({ musicData }: {musicData: StoryResType[]}) {
           <div className={styles.contentWrap}>
             <h3>{music.song}</h3>
             <div className={styles.func}>
-              <span className={styles.heart}><FaRegHeart /></span>
+              <span className={`${styles.heart} ${music.isCollection && styles.isCollect}`}
+                onClick={() => patchCollection(music.rid)}>
+                {music.isCollection ? <FaHeart /> : <FaRegHeart />}
+              </span>
               <YouTubeEmbed embedId={music.link} />
             </div>
           </div>
--- ts/types/MusicList.type.ts
+++ ts/types/MusicList.type.ts
@@ -1,9 +1,3 @@
-export type StoryResType = {
-    thumbnails: string,
-    link: string,
-    song: string,
-}
-
 export type PlayListType = {
     rid: number;
     isCollection: boolean;
